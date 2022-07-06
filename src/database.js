1dc77296971ae6d889ee059b1487a6e56b5ed36f
Ronald <ronald.isael@hotmail.com>
2022-07-05 21:37:42 -0400

    Tiene la conexion a la Base de Datos

--- src/database.js
+++ src/database.js
@@ -1,5 +1,9 @@
 import db from 'mongoose'
 
-db.connect("mongodb://Localhost/first-restapi")
+db.connect("mongodb://Localhost/first-restapi", {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+})
     .then(dab => console.log('DataBase is Conected'))
     .catch(error => console.log(error))
+
