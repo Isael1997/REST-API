8bf9e2eb42b791da24cebd14878e1ec49e3e5945
Ronald <ronald.isael@hotmail.com>
2022-07-13 19:25:18 -0400

    Se Agrego la funcionalidad de SignIn, ya se puede ingresar

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -4,8 +4,27 @@ import config from "../config";
 import role from "../models/roles";
 
 export const SignIn = async (req, res) => {
-    const { username, email, password, roles } = req.body;
-    res.json("Sign In");
+    try {
+        const { username, email, password, roles } = req.body;
+        const userFound = await user.findOne({ email: req.body.email });
+        const matchpassword = await user.comparePassword(req.body.password, userFound);
+
+        if (!userFound) {
+            return res.status(400).json({ message: "User not Found" });
+        }else if (userFound == true){
+            if(!matchpassword){
+                return res.status(401).json({
+                    token: null,
+                    message: "Invalid Password"
+                });
+            }else{
+                const token = jwt.sign({ id: userFound._id }, config.secret, { expiresIn: 86400 });
+                res.status().json({ token });
+            }
+        }         
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 export const SignUp = async (req, res) => {
@@ -17,11 +36,11 @@ export const SignUp = async (req, res) => {
         password: await user.encryptPassword(password),
     });
 
-    if(roles){
-        const foundRole = await role.find({name: {$in: roles}});
+    if (roles) {
+        const foundRole = await role.find({ name: { $in: roles/*This is for re.body.roles */ } });
         newUser.roles = foundRole.map((role) => role._id);
-    }else{
-        const rol = await role.findOne({ name: "user"});
+    } else {
+        const rol/*This is for save of data of the db */ = await role.findOne({ name: "user" });
         newUser.roles = [rol._id];
     }
     //saving of data
@@ -34,5 +53,5 @@ export const SignUp = async (req, res) => {
         config.secret /*THe Word Secret */,
         { expiresIn: 86400 /*The time is 24 hours*/ }
     );
-    res.status(200).json({token: token});
+    res.status(200).json({ token: token });
 };
