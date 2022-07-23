273fb0d708b4af94c4fdec29a7261c64d30b2b03
Ronald <ronald.isael@hotmail.com>
2022-07-22 21:53:14 -0400

    Se realizao la verificacion del roles de moderate

--- src/middlewares/authJwt.js
+++ src/middlewares/authJwt.js
@@ -1,23 +1,47 @@
 import jwt from 'jsonwebtoken'
 import config from '../config'
 import user from '../models/user'
+import Role from '../models/roles'
 
-export const verifyToken = async (req, res, next) => {    
+export const verifyToken = async (req, res, next) => {
     try {
         const token = req.headers["x-access-token"];
         if (!token) {
-            return res.status(403).json({message: "No Token previded"});
+            return res.status(403).json({ message: "No Token previded" });
         }
         const decoded = jwt.verify(token, config.secret);
-        const User = await user.findById(decoded.id);
-        if(!User){
-            return res.status(403).json({message: "User no found"});
+        req.userId = decoded.id;
+
+        const User = await user.findById(req.userId);
+        if (!User) {
+            return res.status(403).json({ message: "User no found" });
         }
         next();
     } catch (error) {
-        console.log(error);     
-        return res.status(404).json({message: "unauthorized"});   
+        console.log(error);
+        return res.status(404).json({ message: "Unauthorized" });
     }
+}
+
+export const isModerate = async (req, res, next) => {
+
+    try {
+        const User = await user.findById(req.userId);
+        const roles = await Role.find({ _id: { $in: User.roles } });
     
-    
+        for (let i = 0; i < roles.length; i++) {
+            if (roles[i].name === "moderate") {
+                next();
+                return;
+            }
+        }
+        return res.status(403).json({ message: "Require Moderator Role!" });
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+
+export const isAdmin = async (req, res, next) => {
+
 }
--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -4,14 +4,14 @@ import {authJwt} from '../middlewares/index'
 
 const router = Router();
 
-router.post('/', authJwt.verifyToken, productCtrl.createProduct);
+router.post('/', [authJwt.verifyToken, authJwt.isModerate], productCtrl.createProduct);
 
-router.get('/', authJwt.verifyToken, productCtrl.getProduct);
+router.get('/',  productCtrl.getProduct);
 
 router.get('/:productId', authJwt.verifyToken, productCtrl.getProductById);
 
 router.put('/:productId', authJwt.verifyToken, productCtrl.updateProductById);
 
-router.delete('/:productId', authJwt.verifyToken, productCtrl.deleteProductById);
+router.delete('/:productId', [authJwt.verifyToken, authJwt.isModerate], productCtrl.deleteProductById);
 
 export default router;
